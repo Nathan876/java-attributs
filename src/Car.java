bd9d32647febc1653e16a275198290052a783888
Nathan876 <sequins.trancheur0c@icloud.com>
2025-02-11 10:24:01 +0100

    Exercice 5

---
+++ src/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    private String brand;
+
+    public Car(String brand) {
+        this.brand = brand;
+    }
+
+    public void showBrand() {
+        System.out.println("La marque de la voiture est :" + brand);
+    }
+}
---
+++ src/Exo5.java
@@ -0,0 +1,8 @@
+
+
+public class Exo5 {
+    public static void main(String[] args) {
+        Car myCar = new Car("DS");
+        myCar.showBrand();
+    }
+}
